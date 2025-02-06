513f497abd5602001670591497a479fab30ff464
ZomCie <rodriguezelijah0327@gmail.com>
2025-02-05 17:12:01 -0800

    Enemy_test Attack
    The squirrel now attacks, but no damage

--- Scripts/Damageable.gd
+++ Scripts/Damageable.gd
@@ -2,10 +2,12 @@ extends Node
 
 class_name Damageable
 
-@export var health : float = 20
+@export var health : float = 30
 
 func hit(damage : int):
 	health -= damage
+	print(health)
 	
 	if (health <= 0):
 		get_parent().queue_free()
+		
--- Scripts/enemy_test.gd
+++ Scripts/enemy_test.gd
@@ -1,15 +1,35 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
+var SPEED = 200.0
 var direction = Vector2(1,0)
 
 const JUMP_VELOCITY = -400.0
 
+@export var damage : int = 10
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _ready():
+	$Direction_Switch/Kill_zone/CollisionShape2D.disabled = true
+	$Direction_Switch/Attack_zone.connect("body_entered",_on_body_entered_attack,CONNECT_PERSIST)
+	$Direction_Switch/Kill_zone.connect("body_entered",_on_body_entered_kill,CONNECT_PERSIST)
 	$Direction_Switch/RayCastDown.enabled = true
 	
+func _on_body_entered_kill(body):
+	if body.name == "Player_Character":
+		print("hit")
+
+func _on_body_entered_attack(body):
+	if body.name == "Player_Character":
+		$AnimationPlayer.play("attack")
+
+func _speed_up():
+	SPEED = 1000
+	
+func _speed_down():
+	SPEED = 200
+
+
 func _physics_process(delta):
 	
 	# Add the gravity.
